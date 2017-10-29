7c42cae6b0d5c87878dc7c03bd74d2ae3f8f0cb4
Waseem Ahmed <alphawaseem@gmail.com>
2017-10-29 10:31:10 +0530

    implement header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,16 @@ import React, { Component } from 'react';
 export class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <header className="top"> 
+        <h1>
+        Catch 
+        <span className="ofThe">
+        <span className="of">of</span><span className="the">the</span>
+        </span>
+        day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
     )
   }
 }
