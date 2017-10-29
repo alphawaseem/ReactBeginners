c4e8cc155457be6cf74315f6196fb0deb1662d77
Waseem Ahmed <alphawaseem@gmail.com>
2017-10-29 17:40:20 +0530

    route to store on form submit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-
+import { getFunName } from '../helpers';
+import { Redirect } from 'react-router-dom';
 
 class StorePicker extends Component {
-  render(){
-    return (
-      <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Enter a store name" />
-        <button type="submit">Visit the store -></button>
-      </form>
-    )
+
+  state = {
+    to: ''
   }
+
+  render() {
+    return this.state.to ? <Redirect to={this.state.to} /> :
+      <form onSubmit={this.visitStore} className="store-selector" >
+        <h2>Please Enter A Store</h2>
+        <input ref={(input) => { this.input = input }} type="text" required placeholder="Enter a store name" defaultValue={getFunName()} />
+        <button type="submit">Visit the store -></button>
+      </form >
+  }
+
+
+
+  visitStore = (event) => {
+    event.preventDefault();
+    this.setState({
+      to: '/store/' + this.input.value
+    });
+  }
+
 }
 
 export default StorePicker;
