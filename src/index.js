bab17b42a7bfa91ac86d70baf3c57fa91b9b8125
Waseem Ahmed <alphawaseem@gmail.com>
2017-10-29 14:41:29 +0530

    setup router

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './components/App';
+import StorePicker from './components/StorePicker';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
-render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+      </div>
+    </Router>
+  )
+}
+
+render(<Root />, document.getElementById('root'));
 registerServiceWorker();
