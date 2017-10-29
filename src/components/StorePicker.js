c412a7c85bf46ee36927d990cfa38c6fc039ca07
Waseem Ahmed <alphawaseem@gmail.com>
2017-10-29 10:01:13 +0530

    setup styles and fonts

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 
 class StorePicker extends Component {
   render(){
-    return <h1>Hello World!</h1>;
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Enter a store name" />
+        <button type="submit">Visit the store -></button>
+      </form>
+    )
   }
 }
 
