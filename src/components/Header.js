cfce667d786d585822346f378b7e0d2ae34ec6a6
Waseem Ahmed <alphawaseem@gmail.com>
2017-10-29 12:57:18 +0530

    change header to stateless component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 
 
-export class Header extends Component {
-  render() {
-    return (
-      <header className="top"> 
-        <h1>
-        Catch 
-        <span className="ofThe">
-        <span className="of">of</span><span className="the">the</span>
+export const Header = ({ tagline }) => {
+  return (
+    <header className="top">
+      <h1>
+        Catch
+      <span className="ofThe">
+          <span className="of">of</span><span className="the">the</span>
         </span>
         day
-        </h1>
-        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-      </header>
-    )
-  }
+      </h1>
+      <h3 className="tagline"><span>{tagline}</span></h3>
+    </header>
+  )
 }
 
 export default Header;
